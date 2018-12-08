7417d25ea3eab27f206199bafc8533c894adb1cb
Vikas KM <vikki.km@gmail.com>
2018-12-08 22:42:42 +0530

    finished the drum but cleanup needed and improvement

--- 01 - JavaScript Drum Kit/drumKit.js
+++ 01 - JavaScript Drum Kit/drumKit.js
@@ -1,3 +1,19 @@
 window.addEventListener('keydown', function(e){
-    console.log(e);
-})
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+    if(!audio)  return;
+    // console.log(audio);
+    audio.currentTime = 0; //for rewind
+    audio.play();
+    key.classList.add('playing');
+});
+
+function removeTransition(e){
+    // console.log(e.propertyName);
+    if(e.propertyName !== 'transform')    return;
+    // console.log(this);  // refers to the key event which it triggered
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
